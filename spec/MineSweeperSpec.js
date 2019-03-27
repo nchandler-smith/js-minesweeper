597b4a96aeadaf7c3cc6b6d201769a10da4dabd2
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-27 10:38:57 -0500

    adds place and reveal mine in cell

--- spec/MineSweeperSpec.js
+++ spec/MineSweeperSpec.js
@@ -1,8 +1,22 @@
-describe("Mine Sweeper", function() {
-    describe("Game Board", function() {
+describe("Mine Sweeper Tests", function() {
+
+    describe("Game Board Tests", function() {
         it("should have one cell", function() {
             let board = new Board();
             expect(board.numberOfCells).toEqual(1);
+        });
+    });
+
+    describe("Cell Tests", function() {
+        it("given no mine in cell when reveal then miss", function() {
+            let cell = new Cell();
+            expect(cell.reveal()).toBeFalsy();
+        });
+
+        it("given placeMine is called, cell should have mine", function() {
+           let cell = new Cell();
+           cell.placeMine();
+           expect(cell.reveal()).toBeTruthy();
         });
     });
 });
--- src/MineSweeper.js
+++ src/MineSweeper.js
@@ -1,3 +1,15 @@
 function Board() {
     this.numberOfCells = 1;
+}
+
+function Cell() {
+    this.hasMine = false;
+}
+
+Cell.prototype.reveal = function() {
+    return this.hasMine;
 };
+
+Cell.prototype.placeMine = function() {
+    this.hasMine = true;
+}
