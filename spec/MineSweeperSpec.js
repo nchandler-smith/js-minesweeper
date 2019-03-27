1974498deca678fcaab82295fb4ae806d2d6cf55
Nate Chandler-Smith <nchandler-smith@pillartechnology.com>
2019-03-27 13:05:02 -0500

    adds game ends when mine reveals

--- spec/MineSweeperSpec.js
+++ spec/MineSweeperSpec.js
@@ -2,13 +2,24 @@ describe("Mine Sweeper Tests", function() {
     describe("Game Board Tests", function() {
         let length = 1;
 
-        it("should have length", function() {
+        it("given square board then number of cells equal to square of length", function() {
             let cell = new Cell();
             let board = new Board(length);
 
             board.addCells(cell);
 
             expect(board.getCells().length).toBe(length);
+        });
+
+        it("given cell with mine when reveal then game ends", function() {
+            let cellLocation = 0;
+            let cell = new Cell();
+            let board = new Board(length);
+            board.addCells(cell);
+            board.addMines();
+            board.revealCell(cellLocation)
+
+           expect(board.isGameFinished()).toBeTruthy();
         });
     });
 
--- src/MineSweeper.js
+++ src/MineSweeper.js
@@ -1,6 +1,7 @@
 function Board(length) {
     this.length = length;
     this.cells = [];
+    this.gameFinished = false;
 }
 
 Board.prototype.addCells = function(cell) {
@@ -13,14 +14,29 @@ Board.prototype.getCells = function() {
     return this.cells;
 };
 
+Board.prototype.addMines = function() {
+    this.cells[0].placeMine();
+};
+
+Board.prototype.revealCell = function(cellIndex) {
+    if(this.cells[cellIndex].reveal) {
+        this.gameFinished = true;
+    }
+};
+
+Board.prototype.isGameFinished = function() {
+    return this.gameFinished;
+};
+
 function Cell() {
     this.hasMine = false;
+
+    Cell.prototype.reveal = function() {
+        return this.hasMine;
+    };
+
+    Cell.prototype.placeMine = function() {
+        this.hasMine = true;
+    };
 }
 
-Cell.prototype.reveal = function() {
-    return this.hasMine;
-};
-
-Cell.prototype.placeMine = function() {
-    this.hasMine = true;
-};
