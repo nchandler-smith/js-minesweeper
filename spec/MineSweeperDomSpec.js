c126bc0c1b5c8e4d8acf4547ec39168a7aabbc32
Ian Abbott <ij.abbott2306@gmail.com>
2019-03-29 11:23:30 -0500

    Working game state message

--- spec/MineSweeperDomSpec.js
+++ spec/MineSweeperDomSpec.js
@@ -1,15 +1,20 @@
 describe("Testing DOM manipulation", function () {
-    let length = 1;
+    const LENGTH = 1;
+    const CLEAR_CHAR = "";
+    const MINE_CHAR = "*";
+    const GAME_IN_PROGRESS_MESSAGE = "Game in progress...";
+    const GAME_WIN_MESSAGE = "Player Wins :)";
+    const GAME_LOSE_MESSAGE = "Player Loses :(";
+    const TEST_DIV_ID = "GameBoard";
+    const TEST_CELL_ID = "Cell";
+    const GAME_STATE_MESSAGE_ID = "GameState";
+
     let board;
     let cell;
     let dom;
-    let testCellId;
-    let testDivId;
 
     beforeEach(function () {
-        testDivId = "GameBoard";
-        testCellId = "Cell";
-        board = new Board(length);
+        board = new Board(LENGTH);
         cell = new Cell();
         board.addCells(cell);
         dom = new DomManipulation();
@@ -23,31 +28,52 @@ describe("Testing DOM manipulation", function () {
     it("when init then have a button with no text", function () {
         const div = document.createElement('div');
         const cell = document.createElement('button');
-        div.id = testDivId;
-        cell.id = testCellId;
+        const gameStateMessage = document.createElement('b');
+        div.id = TEST_DIV_ID;
+        cell.id = TEST_CELL_ID;
+        gameStateMessage.id = "GameState";
+        gameStateMessage.innerHTML = GAME_IN_PROGRESS_MESSAGE;
 
         div.appendChild(cell);
+        div.appendChild(gameStateMessage);
 
-        expect(document.getElementById(testDivId)).toEqual(div);
+        expect(document.getElementById(TEST_DIV_ID)).toEqual(div);
     });
 
     it("given a cell without a mine, when revealed, button has empty text", function () {
-        let clearChar = "";
-        let cellDOM = document.getElementById(testCellId);
+        let cellDOM = document.getElementById(TEST_CELL_ID);
 
         cellDOM.click();
 
-        expect(cellDOM.innerHTML).toEqual(clearChar);
+        expect(cellDOM.innerHTML).toEqual(CLEAR_CHAR);
     });
 
     it("given a cell with a mine, when revealed, button has * as text", function () {
-        let mineChar = "*";
-        let cellDOM = document.getElementById(testCellId);
+        let cellDOM = document.getElementById(TEST_CELL_ID);
 
         board.addMines();
         cellDOM.click();
 
-        expect(cellDOM.innerHTML).toEqual(mineChar);
+        expect(cellDOM.innerHTML).toEqual(MINE_CHAR);
+    });
+
+    it("given a single cell without a mine, when revealed, player wins", function () {
+        let cellDOM = document.getElementById(TEST_CELL_ID);
+        let messageDOM = document.getElementById(GAME_STATE_MESSAGE_ID);
+
+        cellDOM.click();
+
+        expect(messageDOM.innerHTML).toEqual(GAME_WIN_MESSAGE);
+    });
+
+    it("given a single cell without a mine, when revealed, player wins", function () {
+        let cellDOM = document.getElementById(TEST_CELL_ID);
+        let messageDOM = document.getElementById(GAME_STATE_MESSAGE_ID);
+
+        board.addMines();
+        cellDOM.click();
+
+        expect(messageDOM.innerHTML).toEqual(GAME_LOSE_MESSAGE);
     });
 });
 
--- src/MineSweeperDom.js
+++ src/MineSweeperDom.js
@@ -3,16 +3,22 @@ function DomManipulation() {}
 DomManipulation.prototype.init = function(board) {
     const div = document.createElement('div');
     const cell = document.createElement('button');
+    const gameStateMessage = document.createElement('b');
     div.id = "GameBoard";
     cell.id = "Cell";
+    gameStateMessage.id = "GameState";
+    gameStateMessage.innerHTML = "Game in progress...";
 
     div.appendChild(cell);
+    div.appendChild(gameStateMessage);
 
     cell.addEventListener("click", () => {
         if(board.getCells()[0].reveal()) {
             cell.innerHTML = "*";
+            gameStateMessage.innerHTML = "Player Loses :(";
         } else {
             cell.innerHTML = '';
+            gameStateMessage.innerHTML = "Player Wins :)";
         }
     });
 
